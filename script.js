6640adf20ccefddde7b21b350b48a3315cd25882
c8in4 <106409156+c8in4@users.noreply.github.com>
2024-06-16 21:56:50 +1200

    Add math logics

---
+++ script.js
@@ -0,0 +1,38 @@
+let num1 = null;
+let num2 = null;
+let operatorSign = null;
+let numbersToDisplay = "";
+
+function operate(operator, a, b) {
+    switch (operator) {
+        case "+":
+            add(a, b);
+            break;
+        case "-":
+            subtract(a, b);
+            break;
+        case "*":
+            multiply(a, b);
+            break;
+        case "/":
+            divide(a, b);
+            break;
+    }
+}
+
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
