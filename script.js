c0f762812725c3adcff83e16016975a852443fa0
c8in4 <106409156+c8in4@users.noreply.github.com>
2024-06-16 22:01:39 +1200

    Add event listener and event handlers

--- script.js
+++ script.js
@@ -3,6 +3,57 @@ let num2 = null;
 let operatorSign = null;
 let numbersToDisplay = "";
 
+const display = document.querySelector("#display");
+
+const clearButton = document.querySelector("#clear");
+
+const numButtons = [
+    zeroButton = document.querySelector("#zero"),
+    oneButton = document.querySelector("#one"),
+    twoButton = document.querySelector("#two"),
+    threeButton = document.querySelector("#three"),
+    fourButton = document.querySelector("#four"),
+    fiveButton = document.querySelector("#five"),
+    sixButton = document.querySelector("#six"),
+    sevenButton = document.querySelector("#seven"),
+    eightButton = document.querySelector("#eight"),
+    nineButton = document.querySelector("#nine")
+];
+
+numButtons.forEach(button => {
+    button.addEventListener("click", (e) => concatChar(e.target.textContent));
+});
+
+const operatorButtons = [
+    plusButton = document.querySelector("#plus"),
+    minusButton = document.querySelector("#minus"),
+    multiplyButton = document.querySelector("#multiply"),
+    divideButton = document.querySelector("#divide")
+];
+
+operatorButtons.forEach(button => {
+    button.addEventListener("click", (e) => {
+        if (+display.textContent) {
+            num1 = +display.textContent;
+        }
+        operatorSign = e.target.textContent;
+        display.textContent = "";
+        concatChar(e.target.textContent);
+    });
+});
+
+clearButton.addEventListener("click", clearDisplay)
+
+function concatChar(char) {
+    display.textContent += char;
+}
+
+
+function clearDisplay() {
+    display.textContent = "";
+    operatorSign = null;
+}
+
 function operate(operator, a, b) {
     switch (operator) {
         case "+":
