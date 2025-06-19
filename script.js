f7995d5ab089f6426cc1b0e52084b48a47ceaae3
c8in4 <106409156+c8in4@users.noreply.github.com>
2025-06-20 08:11:10 +1200

    Add functions for basic math operations

---
+++ script.js
@@ -0,0 +1,30 @@
+let firstNum, secondNum, operator
+
+function add(a, b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+function operate(num1, num2, op) {
+    switch (op) {
+        case '+':
+            return add(num1, num2)
+        case '-':
+            return subtract(num1, num2)
+        case '*':
+            return multiply(num1, num2)
+        case '/':
+            return divide(num1, num2)
+    }
+}
