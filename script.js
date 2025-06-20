6701e33f1898b1132dd5a395cb798bf64439882a
c8in4 <106409156+c8in4@users.noreply.github.com>
2025-06-20 20:25:33 +1200

    Add and style calculator layout and buttons

--- script.js
+++ script.js
@@ -28,3 +28,24 @@ function operate(num1, num2, op) {
             return divide(num1, num2)
     }
 }
+
+const calculatorDisplay = document.querySelector('#calculatorDisplay')
+
+const calculatorButtons = document.querySelector('#calculatorButtons')
+const buttonsArray = [
+    'C', '%', '/',
+    7, 8, 9, '*',
+    4, 5, 6, '-',
+    1, 2, 3, '+',
+    0, '.', '='
+]
+
+buttonsArray.forEach(createButton)
+
+function createButton(text) {
+    const button = document.createElement('button')
+    button.innerText = text
+    button.dataset.value = text
+    calculatorButtons.appendChild(button)
+}
+
